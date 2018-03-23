98550faab6c62a4b7982beab05404b8eb5e485cb
rosh2012 <37698809+rosh2012@users.noreply.github.com>
2018-03-23 23:45:08 +0530

    Add files via upload

---
+++ interrupt.ino
@@ -0,0 +1,28 @@
+const int ledPin = 2;     
+int ledState = 0;         
+
+void setup() {
+ 
+  pinMode(ledPin, INPUT);
+  // Attach an interrupt to the ISR vector
+  attachInterrupt(0, pin_ISR, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("CALIBRSTION LED COUNT: ");
+  Serial.println(ledState);
+  int n=(ledState/12);
+  Serial.print("KILOWATT= ");
+  Serial.println(n);
+  delay(1000);
+}
+
+void pin_ISR() 
+{
+  if(digitalRead(ledPin)==HIGH)
+  {
+    ledState++;
+  }
+}
+
